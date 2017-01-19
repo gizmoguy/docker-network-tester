f8fd7c7131af733f4db16423f76b4248941a734e
Brad Cowie <brad@gizmoguy.net.nz>
2017-01-19 21:13:17 +1300

    Run lots concurrently dot sh.

---
+++ runlotsconcurrently.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Spin up dockers
+for i in `seq 1 100`; do
+    sudo docker run --rm -d --network=none gizmoguy/network-tester
+done
+
+# Kick off dockers by giving them network
+for id in `sudo docker ps | grep gizmoguy/network-tester | awk '{print $1}'`; do
+    sudo ./ovs-docker-with-dhcp add-port br-wired eth0 "$id"
+done
