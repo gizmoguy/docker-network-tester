b46f5c505368d4eb9bbdab9489566cda3710d47a
Brad Cowie <brad@gizmoguy.net.nz>
2017-01-19 17:18:02 +1300

    Use correct path to ovs-docker-with-dhcp.

--- runlots.sh
+++ runlots.sh
@@ -2,5 +2,5 @@
 
 for i in `seq 1 100`; do
     id=$(sudo docker run --rm -d --network=none gizmoguy/network-tester)
-    sudo ovs-docker-with-dhcp add-port br-wired eth0 "$id"
+    sudo ./ovs-docker-with-dhcp add-port br-wired eth0 "$id"
 done
